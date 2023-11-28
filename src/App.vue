8271a45abb48f5f9c09f8fa9c9cc9c57fb863cc0
Luan Mello <close8821@gmail.com>
2023-11-28 14:34:45 -0300

    Feat | Estruturando Blocos

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <div>
+        <v-img src="/bloco1.jpg">
+          TESTE
+        </v-img>
+      </div>
+
+      <div class="bg-black">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <h1>INSERIR CARDS</H1>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+      </div>
+
+      <div>
+        <v-img src="/bloco3.png">
+        </v-img>
+      </div>
+
+      <div>
+        <v-img src="/bloco4.png">
+        </v-img>
+      </div>
+      <Home />
+      <AppBar />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+import Home from '@/components/Home.vue'
+import AppBar from '@/components/AppBar.vue'
 </script>
---
+++ src/components/AppBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app-bar flat height="200">
+        <v-img src="/logo.png" :class="smAndDown ? '' : 'mb-4 h-75'"></v-img>
+        <v-app-bar-title />
+        <v-btn class="rounded-pill border-app d-flex justify-center align-center"
+            :class="smAndDown ? '' : 'mr-16 mt-10 pa-7'">Consultar Horários</v-btn>
+    </v-app-bar>
+</template>
+
+<style scoped>
+.border-app {
+    border-width: 3px;
+    border-color: black;
+}
+</style>
+
+<script setup>
+import { useDisplay } from 'vuetify';
+
+const { smAndDown } = useDisplay()
+</script>
